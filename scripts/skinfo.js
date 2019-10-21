981eca3c1dcb32e206062b364607b24f91407d4e
Viktor Morin <viktor.morin@vattenfall.com>
2019-10-21 21:56:46 +0200

    tested new layout

--- scripts/skinfo.js
+++ scripts/skinfo.js
@@ -17,3 +17,8 @@ function susbsribeToSkinfo(email, buttonId) {
         }
     });
 }
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
