20d80b77fb3d9f4962200a0e67ff107c80d0974b
Viktor Morin <viktor.morin@vattenfall.com>
2020-03-06 17:53:20 +0100

    added updated version of new homepage

---
+++ new_site/scripts/skinfo.js
@@ -0,0 +1,58 @@
+var countDownComplete = false;
+var slotsValue = 0;
+
+function susbsribeToSkinfo(email) {
+    $.ajax({
+        type: 'POST',
+        url: 'https://skinfo-api.azurewebsites.net/email',
+        data: email,
+        success: function () {
+
+        },
+        error: function () {
+
+        },
+        complete: function () {
+
+        }
+    });
+}
+
+function getAvailableSlots() {
+    $.ajax({
+        type: 'GET',
+        jsonCallback: 'updateAvailableSlots',
+        url: 'https://skinfo-api.azurewebsites.net/availableslot/jsonp',
+        dataType: 'jsonp',
+    })
+}
+
+function updateAvailableSlots(data) {
+    countDownComplete = true;
+    slotsValue = data;
+}
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function doSlotsCountDown() {
+    getAvailableSlots();
+    var slots = 350;
+    while (!countDownComplete) {
+        $('#slots').html(slots);
+        slots = slots - 1;
+        await sleep(1);
+    }
+    while (slots > slotsValue) {
+        $('#slots').html(slots);
+        slots = slots - 1;
+        await sleep(10);
+    }
+    $('#slots').html(slotsValue);
+}
