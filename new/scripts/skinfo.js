8b1cefaf9d811e93e99d30cc16b84f03679e7c39
Viktor Morin <viktor.morin@hotmail.com>
2021-03-18 09:16:24 +0100

    added test button

--- new/scripts/skinfo.js
+++ new/scripts/skinfo.js
@@ -42,6 +42,7 @@ $(document).ready(function () {
             window.location.href = this.dataset.url;
         }
     }
+
     $.ajax({
         type: 'GET',
         url: 'https://api.skinfo.se/information/instagram',
@@ -54,4 +55,36 @@ $(document).ready(function () {
             feed.run();
         }
     });
+
+    // function validateEmail(email) {
+    //     var re = /^[^\s@]+@[^\s@]+$/;
+    //     return re.test(email);
+    // }
+
+    function validateEmail(email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    if (document.getElementById('send-email')) {
+        document.getElementById('send-email-button').onclick = function () {
+            var email = document.getElementById('send-email-input').value;
+            if (validateEmail(email)) {
+                $.ajax({
+                    type: 'POST',
+                    url: 'https://api.skinfo.se/information/email?email=' + email,
+                    contentType: "application/json; charset=utf-8",
+                    error: function (result) {
+                        console.log('error');
+                    },
+                    success: function (result) {
+                        console.log('success');
+                    }
+                });
+            }
+            else {
+
+            }
+        }
+    }
 });
