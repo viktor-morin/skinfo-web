4161413a7742732e992fb0d5a1626fd3f244e2b0
viktor.morin@hotmail.com <viktor.morin@hotmail.com>
2021-04-28 12:02:54 +0200

    added info to work sith seo

--- scripts/searchTexts.js
+++ scripts/searchTexts.js
@@ -1,22 +1,22 @@
 var isSafeToUse = 'Is X safe to use?';
-var isSafeToUseYes = 'Yes, it\'s safe to use';
-var isSafeToUseNo = 'No, it\'s not safe to use';
+var isSafeToUseYes = 'There are currently no concerns for this ingredient';
+var isSafeToUseNo = 'Read about the Y concers listed here';
 
 var isGoodForYourSkin = 'Is X good for you skin?';
-var isGoodForYourSkinYes = 'Yes, it\'s good for your skin';
-var isGoodForYourSkinNo = 'No, it\'s not good for your skin';
+var isGoodForYourSkinYes = 'Yes, this ingredients is free from concerns';
+var isGoodForYourSkinNo = 'Read about the Y concers listed here';
 
 var isDangerous = 'Is X dangerous?';
-var isDangerousNo = 'No, it\'s not dangerous';
-var isDangerousYes = 'Yes, it\'s dangerous';
+var isDangerousNo = 'There are currently no concerns for this ingredient';
+var isDangerousYes = 'Read about the Y concers listed here';
 
 var isBadForYourSkin = 'Is X bad for your skin?';
-var isBadForYourSkinNo = 'No, it\'s not bad for your skin';
-var isBadForYourSkinYes = 'Yes, it\'s bad for your skin';
+var isBadForYourSkinNo = 'There are currently no concerns for this ingredient';
+var isBadForYourSkinYes = 'Read about the Y concers listed here';
 
 var isSafeDuringPregnancy = 'Is X safe during pregnancy?';
-var isSafeDuringPregnancyNo = 'No, it\'s not safe during pregnancy';
-var isSafeDuringPregnancyYes = 'Yes, it\'s safe during pregnanacy';
+var isSafeDuringPregnancyNo = 'Read about the concerns';
+var isSafeDuringPregnancyYes = 'There are currently no concerns regarding pregnancy for this ingredient';
 
 var isBadForSensitiveSkin = 'Is X bad for sensitive skin?';
 var isBadForSensitiveSkinNo = 'No, it\'s not bad for sensitive skin';
@@ -27,8 +27,8 @@ var isExfoliatingNo = 'No, it\'s not exfoliating';
 var isExfoliatingYes = 'Yes, it\'s exfoliating';
 
 var isVegan = 'Is X vegan?';
-var isVeganNo = 'No, it\'s not vegan';
-var isVeganYes = 'Yes, it\'s vegan';
+var isVeganNo = 'No, it\'s not vegan'; //no its not vegans, its ...
+var isVeganYes = 'Yes, it\'s vegan'; //yes...
 
 var isGoodForDrySkin = 'Is X good for dry skin?';
 var isGoodForDrySkinNo = 'No, it\'s not good for dry skin';
@@ -56,7 +56,15 @@ var concernsHeader = 'Concerns';
 var originsHeader = 'Origins';
 var settingsHeader = 'Settings';
 
-var documentTitle = 'change X to ingredient - document-title';
-var documentTitleWidget = 'document-Title-Widget';
-var documentDescription = 'change X to ingredient - document-description';
-var documentDescriptionWidget = 'document-Description-Widget';
+var documentTitle = 'What Is X? Facts, Side Effects, Origin And More';
+var documentTitleWidget = 'Ingredient List (Explained) - Skinfo';
+var documentDescription = 'synonyms, functions, concerns.... short';
+var documentDescriptionWidget = 'See the full ingredient list explained by Skinfo';
+
+//Add preferences?
+//Your settings will be remembered in shops where Skinfo is integrated
+
+//Frequently asked questions about X
+//Bra, Dåliga, hudtyp etc
+
+//concerns per ingrediens + widget ska fram
